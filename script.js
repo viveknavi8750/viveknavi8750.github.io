58d48153760d94a8f0fe7acd7ca192f614636e6d
vivek <viveknavi143@gmail.com>
2025-08-18 14:24:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ const observer = new IntersectionObserver(entries => {
 elements.forEach(el => observer.observe(el));
 
 
+
+  const buttons = document.querySelectorAll(".know-more1");
+  const popup = document.getElementById("popup");
+  const popupInner = document.getElementById("popupInner");
+  const backBtn = document.getElementById("backBtn");
+
+  buttons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      const fullInfo = e.target.nextElementSibling; // grab hidden .full-info
+      popupInner.innerHTML = fullInfo.innerHTML;    // insert it into popup
+      popup.style.display = "flex";
+    });
+  });
+
+  backBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
